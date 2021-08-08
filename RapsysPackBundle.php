482e4bd6f7308147b1f0cdcb3c291a167fba78e8
Raphaël Gertz <git@rapsys.eu>
2021-08-08 14:22:01 +0200

    Add strict types Improve documentation

--- RapsysPackBundle.php
+++ RapsysPackBundle.php
@@ -1,7 +1,19 @@
-<?php
+<?php declare(strict_types=1);
+
+/*
+ * This file is part of the Rapsys PackBundle package.
+ *
+ * (c) Raphaël Gertz <symfony@rapsys.eu>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 namespace Rapsys\PackBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
+/**
+ * {@inheritdoc}
+ */
 class RapsysPackBundle extends Bundle {}
