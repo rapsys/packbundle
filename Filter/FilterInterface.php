b8f0779faee6db6d4c4fe71fc417415968966e8f
Raphaël Gertz <git@rapsys.eu>
2021-08-08 14:32:32 +0200

    New tree layout Add strict types Improve documentation Cleanup

--- Filter/FilterInterface.php
+++ Filter/FilterInterface.php
@@ -1,9 +1,27 @@
-<?php
+<?php declare(strict_types=1);
 
-// src/Rapsys/PackBundle/Twig/Filter/FilterInterface.php
-namespace Rapsys\PackBundle\Twig\Filter;
+/*
+ * This file is part of the Rapsys PackBundle package.
+ *
+ * (c) Raphaël Gertz <symfony@rapsys.eu>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
+namespace Rapsys\PackBundle\Filter;
+
+/**
+ * Filter interface definition
+ *
+ * @todo do we need something else ? (like a constructor that read parameters or else)
+ */
 interface FilterInterface {
-    //TODO: see if we need something else (like a constructor that read parameters or something else ?)
-    public function process($content);
+	/**
+	 * Process function
+	 *
+	 * @param string $content The content to process
+	 * @return string The processed content
+	 */
+	public function process(string $content): string;
 }
