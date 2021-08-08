6bc3a323095049fba5ac8bb2b1a2cef1e82b8df5
Raphaël Gertz <git@rapsys.eu>
2021-08-08 14:12:09 +0200

    Version 0.2.0 Add strict types Improve documentation New filters path New tree layout Cleanup

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -1,4 +1,13 @@
-<?php
+<?php declare(strict_types=1);
+
+/*
+ * This file is part of the Rapsys PackBundle package.
+ *
+ * (c) Raphaël Gertz <symfony@rapsys.eu>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 namespace Rapsys\PackBundle\DependencyInjection;
 
@@ -9,34 +18,20 @@ use Symfony\Component\Process\ExecutableFinder;
 /**
  * This is the class that validates and merges configuration from your app/config files.
  *
- * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/configuration.html}
+ * @link http://symfony.com/doc/current/cookbook/bundles/configuration.html}
+ *
+ * {@inheritdoc}
  */
 class Configuration implements ConfigurationInterface {
 	/**
 	 * {@inheritdoc}
 	 */
-	public function getConfigTreeBuilder() {
+	public function getConfigTreeBuilder(): TreeBuilder {
 		//Get TreeBuilder object
 		$treeBuilder = new TreeBuilder('rapsys_pack');
 
 		//Get ExecutableFinder object
 		$finder = new ExecutableFinder();
-
-		/**
-		 * XXX: Note about the output schemes
-		 *
-		 * The output files are written based on the output.<ext> scheme with the * replaced by the hashed path of packed files
-		 *
-		 * The following service configuration make twig render the output file path with the right '/' basePath prefix:
-		 * services:
-		 *     assets.pack_package:
-		 *         class: Rapsys\PackBundle\Asset\PathPackage
-		 *         arguments: [ '/', '@assets.empty_version_strategy', '@assets.context' ]
-		 *     rapsys_pack.twig.pack_extension:
-		 *         class: Rapsys\PackBundle\Twig\PackExtension
-		 *         arguments: [ '@file_locator', '@service_container', '@assets.pack_package' ]
-		 *         tags: [ twig.extension ]
-		 */
 
 		//The bundle default values
 		$defaults = [
@@ -44,7 +39,7 @@ class Configuration implements ConfigurationInterface {
 				'name' => 'asset_url',
 				'scheme' => 'https://',
 				'timeout' => (int)ini_get('default_socket_timeout'),
-				'agent' => (string)ini_get('user_agent')?:'rapsys_pack/0.1.8',
+				'agent' => (string)ini_get('user_agent')?:'rapsys_pack/0.2.0',
 				'redirect' => 5
 			],
 			'output' => [
@@ -55,7 +50,7 @@ class Configuration implements ConfigurationInterface {
 			'filters' => [
 				'css' => [
 					0 => [
-						'class' => 'Rapsys\PackBundle\Twig\Filter\CPackFilter',
+						'class' => 'Rapsys\PackBundle\Filter\CPackFilter',
 						'args' => [
 							$finder->find('cpack', '/usr/local/bin/cpack'),
 							'minify'
@@ -64,7 +59,7 @@ class Configuration implements ConfigurationInterface {
 				],
 				'js' => [
 					0 => [
-						'class' => 'Rapsys\PackBundle\Twig\Filter\JPackFilter',
+						'class' => 'Rapsys\PackBundle\Filter\JPackFilter',
 						'args' => [
 							$finder->find('jpack', '/usr/local/bin/jpack'),
 							'best'
@@ -73,19 +68,23 @@ class Configuration implements ConfigurationInterface {
 				],
 				'img' => [
 					0 => [
-						'class' => 'Rapsys\PackBundle\Twig\Filter\IPackFilter',
+						'class' => 'Rapsys\PackBundle\Filter\IPackFilter',
 						'args' => []
 					]
 				],
 			]
 		];
 
-		//Here we define the parameters that are allowed to configure the bundle.
-		//XXX: see https://github.com/symfony/symfony/blob/master/src/Symfony/Bundle/FrameworkBundle/DependencyInjection/Configuration.php for default value and description
-		//XXX: see http://symfony.com/doc/current/components/config/definition.html
-		//XXX: see https://github.com/symfony/assetic-bundle/blob/master/DependencyInjection/Configuration.php#L63
-		//XXX: see php bin/console config:dump-reference rapsys_pack to dump default config
-		//XXX: see php bin/console debug:config rapsys_pack to dump config
+		/**
+		 * Defines parameters allowed to configure the bundle
+		 *
+		 * @link https://github.com/symfony/symfony/blob/master/src/Symfony/Bundle/FrameworkBundle/DependencyInjection/Configuration.php
+		 * @link http://symfony.com/doc/current/components/config/definition.html
+		 * @link https://github.com/symfony/assetic-bundle/blob/master/DependencyInjection/Configuration.php#L63
+		 *
+		 * @see php bin/console config:dump-reference rapsys_pack to dump default config
+		 * @see php bin/console debug:config rapsys_pack to dump config
+		 */
 		$treeBuilder
 			//Parameters
 			->getRootNode()
@@ -113,7 +112,11 @@ class Configuration implements ConfigurationInterface {
 						->addDefaultsIfNotSet()
 						->children()
 							->arrayNode('css')
-								#XXX: undocumented, see Symfony/Component/Config/Definition/Builder/ArrayNodeDefinition.php +513
+								/**
+								 * Undocumented
+								 *
+								 * @see Symfony/Component/Config/Definition/Builder/ArrayNodeDefinition.php +513
+								 */
 								->addDefaultChildrenIfNoneSet()
 								->arrayPrototype()
 									->children()
@@ -123,8 +126,8 @@ class Configuration implements ConfigurationInterface {
 											->defaultValue($defaults['filters']['css'][0]['class'])
 										->end()
 										->arrayNode('args')
-											/*->isRequired()*/
-											->treatNullLike(array())
+											//->isRequired()
+											->treatNullLike([])
 											->defaultValue($defaults['filters']['css'][0]['args'])
 											->scalarPrototype()->end()
 										->end()
@@ -132,7 +135,11 @@ class Configuration implements ConfigurationInterface {
 								->end()
 							->end()
 							->arrayNode('js')
-								#XXX: undocumented, see Symfony/Component/Config/Definition/Builder/ArrayNodeDefinition.php +513
+								/**
+								 * Undocumented
+								 *
+								 * @see Symfony/Component/Config/Definition/Builder/ArrayNodeDefinition.php +513
+								 */
 								->addDefaultChildrenIfNoneSet()
 								->arrayPrototype()
 									->children()
@@ -142,7 +149,7 @@ class Configuration implements ConfigurationInterface {
 											->defaultValue($defaults['filters']['js'][0]['class'])
 										->end()
 										->arrayNode('args')
-											->treatNullLike(array())
+											->treatNullLike([])
 											->defaultValue($defaults['filters']['js'][0]['args'])
 											->scalarPrototype()->end()
 										->end()
@@ -150,7 +157,11 @@ class Configuration implements ConfigurationInterface {
 								->end()
 							->end()
 							->arrayNode('img')
-								#XXX: undocumented, see Symfony/Component/Config/Definition/Builder/ArrayNodeDefinition.php +513
+								/**
+								 * Undocumented
+								 *
+								 * @see Symfony/Component/Config/Definition/Builder/ArrayNodeDefinition.php +513
+								 */
 								->addDefaultChildrenIfNoneSet()
 								->arrayPrototype()
 									->children()
@@ -160,7 +171,7 @@ class Configuration implements ConfigurationInterface {
 											->defaultValue($defaults['filters']['img'][0]['class'])
 										->end()
 										->arrayNode('args')
-											->treatNullLike(array())
+											->treatNullLike([])
 											->defaultValue($defaults['filters']['img'][0]['args'])
 											->scalarPrototype()->end()
 										->end()
