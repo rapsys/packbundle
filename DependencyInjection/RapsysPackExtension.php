f6d353c024df3404a0248e70b7e44c4828412e79
Raphaël Gertz <git@rapsys.eu>
2021-08-08 14:17:14 +0200

    Add strict types Improve documentation

--- DependencyInjection/RapsysPackExtension.php
+++ DependencyInjection/RapsysPackExtension.php
@@ -1,4 +1,13 @@
-<?php
+<?php declare(strict_types=1);
+
+/*
+ * This file is part of the Rapsys PackBundle package.
+ *
+ * (c) Raphaël Gertz <symfony@rapsys.eu>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 namespace Rapsys\PackBundle\DependencyInjection;
 
@@ -9,12 +18,14 @@ use Symfony\Component\DependencyInjection\Extension\Extension;
  * This is the class that loads and manages your bundle configuration.
  *
  * @link http://symfony.com/doc/current/cookbook/bundles/extension.html
+ *
+ * {@inheritdoc}
  */
 class RapsysPackExtension extends Extension {
 	/**
 	 * {@inheritdoc}
 	 */
-	public function load(array $configs, ContainerBuilder $container) {
+	public function load(array $configs, ContainerBuilder $container): void {
 		//Load configuration
 		$configuration = $this->getConfiguration($configs, $container);
 
@@ -34,7 +45,7 @@ class RapsysPackExtension extends Extension {
 	/**
 	 * {@inheritdoc}
 	 */
-	public function getAlias() {
+	public function getAlias(): string {
 		return 'rapsys_pack';
 	}
 }
